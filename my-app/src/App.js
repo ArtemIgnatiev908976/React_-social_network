5abf8656bd6f66fb6492a4f6a3261d2b1f549a83
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-03-30 16:01:44 +0400

    Lesson 6

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+      <div className = "App" > //стрелочная функция возращает разметку
+
+        <ul>
+         <li >123 < /li>
+         <li> 231< /li >
+        </ul>
+
+        </div>
+    );
 }
 
 export default App;
