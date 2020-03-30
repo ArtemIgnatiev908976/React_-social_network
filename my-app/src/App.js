a71d4571b4a392843dc4036baa23f971cef496e8
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-03-30 19:49:01 +0400

    Create comp Footer add plugin for import
    Lesson 10

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js'
-import Technologies from './Technologies.js'
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+import Footer from './Footer.js';
 
 const App = () => {
-    return (<div>
-      <Header />
-      <Technologies />
-      </div>
-
-
+    return (
+        <div>
+        <Header / >
+        <Technologies / >
+        <Footer />
+        </div>
     );
 }
 
