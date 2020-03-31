944da848075464415fb17304775dff299c2e02a0
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-03-31 20:33:47 +0400

    Create components lesson 12
    Разбили страничку на 3 компанента, вынесли их в отдельную папку

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js';
+import Header from './components/Header.jsx';
+import Navbar from './components/Navbar.jsx';
+import Profile from './components/Profile.jsx';
 import Technologies from './Technologies.js';
-import Footer from './Footer.js';
+
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-        <header className='header'>
 
-
-        </header>
-        <nav className='nav'>
-          <div><a>Profile</a></div>
-          <div><a>Messages</a></div>
-          <div><a>News</a></div>
-          <div><a>Music</a></div>
-          <div><a>Settings</a></div>
-        </nav>
-        <div className='content'>Main content
-        <img src="https://miro.medium.com/max/2400/1*d6l-K0K-4U8ROEXG9_Ox7Q.png" />
-
-        <div><a>desc+ava</a></div>
-        <div><a>my posts</a></div>
-        <div><a>New posts</a></div>
-        <div><a>posts1</a></div>
-        <div><a>posts2</a></div>
-
-        </div>
-
+        <Header />
+        <Navbar />
+        <Profile />
 
         </div>
     );
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Header = () =>{
+
+  return   <header className='header'>
+  12312222222222
+           </header>
+}
+
+export default Header;
