d559604b37d2ca5fb4bdd2b1b4e7446e806362ba
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-03-31 23:51:01 +0400

    create modules for components lesson 14

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import s from './Header.module.css';
 
 
 const Header = () =>{
 
-  return   <header className='header'>
-  12312222222222
+  return   <header className={s.header}>
+  <img src="https://img.rg.ru/img/content/174/48/56/DB2019AL01916_large_d_850.jpg" />
            </header>
 }
 
--- my-app/src/components/Profile.jsx
+++ my-app/src/components/Profile.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import s from './Profile.module.css';
 
 const Profile = () =>{
 
-  return  <div className='content'>Main content
+  return  <div className={s.content}>Main content
   <img src="https://miro.medium.com/max/2400/1*d6l-K0K-4U8ROEXG9_Ox7Q.png" />
 
   <div><a>desc+ava</a></div>
