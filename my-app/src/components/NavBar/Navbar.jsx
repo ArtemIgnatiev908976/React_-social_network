43d278070c125464656e5fa1409b79f993680457
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-02 17:27:47 +0400

    lesson 20
    Добавили навигацию Navlink

--- my-app/src/components/NavBar/Navbar.jsx
+++ my-app/src/components/NavBar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 
 const Navbar = () =>{
 
   return  <nav className={s.nav}>
-    <div className={s.item}><a href='/profile'>Profile</a></div>
-    <div className={s.item}><a href='/dialogs'>Messages</a></div>
-    <div className={s.item}><a href='/profile'>News</a></div>
-    <div className={s.item}><a href='/profile'>Music</a></div>
-    <div className={s.item}><a href='/profile'>Settings</a></div>
+    <div className={s.item}><NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink></div>
+    <div className={s.item}><NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink></div>
+    <div className={s.item}><NavLink to='/profile'>News</NavLink></div>
+    <div className={s.item}><NavLink to='/profile'>Music</NavLink></div>
+    <div className={s.item}><NavLink to='/profile'>Settings</NavLink></div>
   </nav>
 }
 
