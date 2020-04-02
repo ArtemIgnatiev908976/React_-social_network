ac0c596eaca2062ae63d985d16b42a9057458341
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-02 17:48:56 +0400

    lesson 21
    довавили диалоги

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import s from './Dialogs.module.css';
 
-const Dialogs =(props) =>{
+const Dialogs = (props) => {
 
-    return(
-        <div className={s.content}>
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog+' '+s.active}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Dima1
+                </div>
+                <div className={s.dialog}>
+                    Dima2
+                </div>
+                <div className={s.dialog}>
+                    Dima3
+                </div>
 
-            Dialogs
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>who is you</div>
+                <div className={s.message}>yo</div>
+
+            </div>
         </div>
+
     )
 }
 
