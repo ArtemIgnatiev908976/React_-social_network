08c77e5d1b7d7d5bc2f453d41efe5e21ad413506
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-02 15:22:22 +0400

    lesson 15
    Разбивка компонентов  по папкам

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () =>{
 
@@ -7,10 +8,7 @@ const Profile = () =>{
   <img src="https://miro.medium.com/max/2400/1*d6l-K0K-4U8ROEXG9_Ox7Q.png" />
 
   <div><a>desc+ava</a></div>
-  <div><a>my posts</a></div>
-  <div><a>New posts</a></div>
-  <div><a>posts1</a></div>
-  <div><a>posts2</a></div>
+  <MyPosts />
 
   </div>
 }
