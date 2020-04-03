2bc6728170ec43024eb15cb6935fac011a224ebf
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-03 17:46:32 +0400

    lesson 31
    добавили взаимодействие пользователя с textarea

--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -14,10 +14,22 @@ const MyPosts = (props) => {
 
     let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
+    let newPostElement = React.createRef();
+
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text)
+    }
+
+
     return <div className={s.postsBlock}>
-        <div><textarea name="" id="" cols="30" rows="10"></textarea></div>
         <div>
-            <button>Add post</button>
+            <textarea ref={newPostElement}></textarea>
+        </div>
+        <div>
+            <button onClick={addPost}>Add post
+            </button>
         </div>
 
         <div className={s.posts}>
