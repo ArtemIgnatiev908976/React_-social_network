a7568d0796838eb79eee404445f34a611828bed3
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-03 18:45:04 +0400

    lesson 33
    Реализовали функционал добавления поста на страницу профиля

---
+++ my-app/src/render.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import {addPost} from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
+
+
+// addPost('hey');
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App state={state} addPost={addPost} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+
