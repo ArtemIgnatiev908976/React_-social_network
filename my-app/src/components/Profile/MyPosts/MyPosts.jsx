9679ac8604238ac99a007459a72dd7b0e178bf17
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-05 15:54:29 +0400

    lesson 41
    reducer раздробили фонкции по отдельным файлам reducer

--- my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import {appPostActionCreator, updateNewPostActionCreator} from "../../../redux/state";
+import {appPostActionCreator, updateNewPostActionCreator} from "../../../redux/profile-reducer";
 
 
 
